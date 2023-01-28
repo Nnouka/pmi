8698cf688dc00539ba5cff01a5b669999a9f7dcb
Nnouka Stephen <stephen.nnouka@joza-it.fr>
2023-01-28 18:17:52 +0100

    add mobile responsiveness

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="zxx">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="author" content="mungwin.com">
 
   <title>Derm Tech FR - @yield('title')</title>
