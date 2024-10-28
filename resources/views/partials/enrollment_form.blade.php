fa1632b69bbf9db295e81bc21c193f020aa8ca19
Fuanyi Jude <fuanyijude@gmail.com>
2024-10-28 16:10:50 +0100

    adding enrollment_form on partial

---
+++ resources/views/partials/enrollment_form.blade.php
@@ -0,0 +1,37 @@
+<!-- resources/views/partials/enrollment_form.blade.php -->
+<div class="modal fade" id="enrollmentModal" tabindex="-1" aria-labelledby="enrollmentModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="enrollmentModalLabel">Enroll Now</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('enroll.submit') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="firstName" class="form-label">First Name</label>
+                        <input type="text" class="form-control" id="firstName" name="first_name" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="lastName" class="form-label">Last Name</label>
+                        <input type="text" class="form-control" id="lastName" name="last_name" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="congregation" class="form-label">Name of Congregation</label>
+                        <input type="text" class="form-control" id="congregation" name="congregation" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="phone" class="form-label">Phone Contact</label>
+                        <input type="tel" class="form-control" id="phone" name="phone" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
