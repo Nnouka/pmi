ce48d8a25b05c1e1266a46c24487f3be50c5c51f
nnoukastephen <nnoukastephen@gmail.com>
2024-11-11 11:31:15 +0100

    update mission.blade

--- resources/views/partials/about/mission.blade.php
+++ resources/views/partials/about/mission.blade.php
@@ -1,5 +1,5 @@
 @include('partials.about.section', [
-    'title' => 'OUR MISSION STATEMENT', 
+    'title' => 'OUR VISION AND MISSION', 
     'content' => 'It is our earnest desire to tow the line of strengthening the Congregations of Church Of Christ globally. We therefore aim at helping sister Congregations of the Lord\'s Church to attain to the scriptural measure of the stature of the fullness of Christ - the only begotten Son of God - Eph. 4:13.'
     ]
 )
